e6b9e20f402767d535896b1bf49a88a3763ed3e7
Zane Rockenbaugh <zane@liquid-labs.com>
2019-03-24 11:32:22 -0500

    polished the input theme to make the 'view' mode labels more prominent and increase data font size. Also, use the nicer 'spread' transition on the underline more consistently

--- js/theme.js
+++ js/theme.js
@@ -46,47 +46,97 @@ const configuredColorScheme = (envKey) => {
   }
 }
 
-const standardCatalystThemeOverrides = (palette) => ({
-  // TODO: the color settings work if only 'main' color is specified.
-  MuiTooltip : {
-    popper : {
-      '@media print' : {
-        // Without this, the invisible tooltip messes up printing
-        display : 'none'
-      }
-    }
-  },
-  MuiInput : {
-    input : {
-      padding : '4px 0 5px' // slightly more compact; was 6 0 7
-    }
-  },
-  MuiMenuItem : {
-    root : {
-      "&$selected" : {
-        "&:hover" : {
-          "backgroundColor" : palette.primary.main
-        },
-        "&" : {
-          "backgroundColor" : palette.primary.light
+const standardCatalystThemeOverrides = (palette) => {
+  const light = palette.type === 'light'
+  // a little lighter (and then darker) than the default
+  const bottomLineColorView = light ? 'rgba(0, 0, 0, 0.10)' : 'rgba(255, 255, 255, 0.92)'
+  // the defaults at the time of writing
+  const bottomLineColorEdit = light ? 'rgba(0, 0, 0, 0.42)' : 'rgba(255, 255, 255, 0.7)'
+  return {
+    // TODO: the color settings work if only 'main' color is specified.
+    MuiTooltip : {
+      popper : {
+        '@media print' : {
+          // Without this, the invisible tooltip messes up printing
+          display : 'none'
         }
       }
     },
-    selected : {}
-  },
-  MuiListItem : {
-    button : {
-      '&:hover' : {
-        backgroundColor : palette.primary.main
+    MuiInput : {
+      input : {
+        padding  : '4px 0 2px', // more compact; was 6 0 7
+        fontSize : 'calc(1em + 2px)', // and enlarge font
+      },
+      underline : {
+        // Implement 'expanding effect' that the default uses for '$focused' on
+        // :hover as well. It looks better, and why have two effects anyway?
+        '&:hover:after' : {
+          transform : 'scaleX(1)',
+        },
+        '&:before' : {
+          // a bit bolder
+          borderBottom : `1px solid ${bottomLineColorEdit}`,
+          // longer, linear transition on colors
+          transition   : 'border-bottom-color 375ms linear 0ms',
+        },
+        '&:hover:not($disabled):not($focused):not($error):before' : {
+          // This is the other half of getting the 'expanding effect' on :hover.
+          // It overrides the default hover rule.
+          borderBottom : 'inherit',
+        },
+        '&$disabled:before' : {
+          borderBottomStyle : 'solid', // intead of dotted
+          // use our less-contrasting color
+          borderBottom      : `1px solid ${bottomLineColorView}`,
+        },
+      },
+    },
+    MuiInputLabel : {
+      // Our 'disabled' input is a 'view' input and so we want to highlight the
+      // label, rather than diminish it.
+      root : {
+        // It would be preferrable to leave bold only for view (disabled) mode,
+        // but 'font-weight' does not transition smoothly, so we hold it
+        // constant.
+        fontWeight   : 'bold',
+        // and reduce contrast to compensate for the boldness
+        color        : `${palette.type === 'light' ? 'rgba(0,0,0,.36)' : 'rgba(255,255,255,.80)' }`,
+        '&$disabled' : {
+          color : `${palette.type === 'light' ? palette.primary.dark : palette.primary.light }`,
+        },
+        '&$animated' : {
+          // use the slow, linear color transform
+          transition : 'color 375ms linear 0ms',
+        }
+      },
+    },
+    MuiMenuItem : {
+      root : {
+        "&$selected" : {
+          "&:hover" : {
+            "backgroundColor" : palette.primary.main
+          },
+          "&" : {
+            "backgroundColor" : palette.primary.light
+          }
+        }
+      },
+      selected : {}
+    },
+    MuiListItem : {
+      button : {
+        '&:hover' : {
+          backgroundColor : palette.primary.main
+        }
+      }
+    },
+    MuiSnackbarContent : {
+      root : {
+        "flexWrap" : "nowrap"
       }
     }
-  },
-  MuiSnackbarContent : {
-    root : {
-      "flexWrap" : "nowrap"
-    }
   }
-})
+}
 
 const standardCatalystThemeTypography = (palette) => ({
   h3 : {
@@ -98,8 +148,9 @@ const standardCatalystThemeTypography = (palette) => ({
 
 const createCatalystTheme = (themeSpec) => {
   const palette = (themeSpec && themeSpec.palette) || {}
-  if (!palette.primary) {palette.primary = configuredColorScheme('PRIMARY')}
-  if (!palette.secondary) {palette.secondary = configuredColorScheme('SECONDARY')}
+  if (!palette.primary) palette.primary = configuredColorScheme('PRIMARY')
+  if (!palette.secondary) palette.secondary = configuredColorScheme('SECONDARY')
+  if (!palette.type) palette.type = 'light'
   // TODO: document our convention for confirm, light, and dark to use light for
   // background, dark for borders, and main for icons. 'contrastLight' is
   // therefore the standard text color (on a 'light' background).
